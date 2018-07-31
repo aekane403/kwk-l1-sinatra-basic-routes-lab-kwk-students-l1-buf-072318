c01926a739c5fad0e7405c2752e8c58202d376d9
Abigail Kane <aekane403@gmail.com>
2018-07-31 19:06:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    "tana"
+end
+  get '/hometown'
+  "susville"
+end 
+  get '/favorite-song'
+  "Our Song"
+end 
 end
